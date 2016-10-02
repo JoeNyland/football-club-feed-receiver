2b9e831c3557605d0e35f35cff7077a6211c53bf
Joe Nyland <joenyland@me.com>
2016-10-02 20:23:42 +0100

    Import DBI

---
+++ lib/DBI.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Class DBI
+ *
+ * This is an abstract layer above the database, allowing the application to be DB type agnostic.
+ */
+class DBI {
+
+    /**
+     * Stores a fixture event in the DB.
+     * Provides 'upsert' functionality to allow a new model to be stored, or an existing model to be updated with new
+     * values.
+     *
+     * @param \Model\Fixture $fixture A Fixture object to persist
+     */
+    public function store_fixture(\Model\Fixture $fixture) {
+        // ToDo: Complete this method
+        error_log('Storing a fixure');
+    }
+
+    /**
+     * Stores a fixture event in the DB.
+     * Provides 'upsert' functionality to allow a new model to be stored, or an existing model to be updated with new
+     * values.
+     *
+     * @param \Model\MatchReport $match_report A MatchReport object to persist
+     */
+    public function store_match_report(\Model\MatchReport $match_report) {
+        // ToDo: Complete this method
+        error_log('Storing a match report');
+    }
+
+}
